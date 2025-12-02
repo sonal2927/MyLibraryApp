6414eb1ff5976012e926b3a200c7b3c4946eaa9b
Sonal <sonalgohil027@gmail.com>
2025-12-02 16:34:59 +0530

    Initial commit

--- obj/Debug/net8.0/MyLibrary.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a79c694ad1a58a18530deb44624569ed77889b53")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
