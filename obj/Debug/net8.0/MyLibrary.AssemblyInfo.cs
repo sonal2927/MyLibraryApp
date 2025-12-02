051fdabb42997f77ff4733f939d4312e41f1de36
Sonal <sonalgohil027@gmail.com>
2025-12-02 22:51:42 +0530

    add Dockerfile for render deploy

--- obj/Debug/net8.0/MyLibrary.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99dd64d735db1629848033cf734a9ddcb431a380")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6734089161f9bfdb7bfebea70092d022ad560db")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
